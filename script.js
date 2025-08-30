5d6de2c60ac7d85f7c98575de9dca1884b27f192
Ronith <ronith.xa@gmail.com>
2025-08-30 17:23:45 +0530

    deploy

---
+++ script.js
@@ -0,0 +1,83 @@
+let choice = document.querySelectorAll(".choice");
+let reset = document.querySelector("button");
+let computer= document.querySelector(".computer");
+let result = document.querySelector(".res");
+let userwin = false;
+let draw = true
+choice.forEach((value,index) =>
+{
+    value.addEventListener("click",() =>
+    {
+        let comopt =computerRandom();
+        check(index, comopt);
+        printResult(comopt);
+    })
+})
+let computerRandom =() =>
+{
+    return Math.floor(Math.random()*3);
+};
+let check =(i,c)=>
+{
+if(i==c)
+{
+    draw=true;
+    return;
+}
+if((i==0 && c==2)||(i==1 && c==0)||(i==2 && c==1) )
+{
+    userwin=true;
+    draw =false
+}
+else
+{
+    userwin =false;
+    darw=false;
+}
+}
+let printResult =(comopt)=>
+{
+    switch(comopt)
+    {
+        case 0 : {computer.innerText ="Computer Choice :  Rock ";break;}
+        case 1 : {computer.innerText ="Computer Choice :  Paper ";break;}
+        case 2 : {computer.innerText ="Computer Choice :  Scissor ";break;}
+
+    }
+    
+    
+    
+    if(draw)
+    {
+        result.innerText = " Game Draw";
+    }
+    else{
+        if(userwin)
+        {
+            result.innerText = "You Win ";
+        }
+        else
+            result.innerText = "You Lose ";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+reset.onclick=() =>
+{
+    computer.innerText = "Computer Choice : ";
+    result.innerText="Result : ";
+};
